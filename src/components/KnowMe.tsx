4db4cca1d52ac8471eaeb6c3ff9ed8582ac0ed55
khoime <j.m.tejada@hotmail.com>
2025-04-09 09:14:22 -0500

    feat: grammar correction

--- src/components/KnowMe.tsx
+++ src/components/KnowMe.tsx
@@ -9,12 +9,12 @@ function KnowMe() {
                         <p className='font-content mx-8 text-[#888888] underline underline-offset-4' >^Joy over stress:</p>
                         <h1 className='font-title mx-8 md:text-4xl sm:text-3xl text-3xl py-2' id="whoami">Life's more than time passing by</h1>
                         <p className='font-content-sm mx-8 md:text-xl text-2xl' >
-                            I've wrote my way into knowledge so that I could experience the joy of being productive and sort of believe in wonderful experiences and
+                            I've written my way to knowledge so that I could experience the joy of being productive and believe in wonderful experiences and
                             how I'll soon become more aware of where I am.
                             <a href="">
                             </a>
-                            Life can be stressful sometimes, we need to breathe, stay calm and carefully watch
-                            and inspect our environment with such joy that we can think of solutions without the weight of stress...
+                            Life can be tough sometimes, we need to breathe, stay calm and carefully watch
+                            and inspect our environment with such a joy that we can think of solutions despite of stress...
                         </p>
                     </div>
                 </div>
